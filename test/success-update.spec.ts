fb9b360e2572245a18b975bd95aca26a94c818f4
Dominique Müller <dominique.m.mueller@gmail.com>
2017-09-23 19:06:11 +0200

    Add initial test file

---
+++ test/success-update.spec.ts
@@ -0,0 +1,38 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+import * as del from 'del';
+import * as git from 'simple-git';
+
+describe( 'Automatic Release', () => {
+
+    const projectPath: string = path.resolve( process.cwd(), 'dist-test' );
+
+    // Setup
+    beforeAll( async() => {
+
+        await del( projectPath );
+        fs.mkdirSync( projectPath );
+        fs.writeFileSync( path.resolve( projectPath, 'package.json' ), JSON.stringify( {
+            name: 'test-library',
+            description: 'Lorem ipsum dolor sit amet.',
+            version: '1.0.0',
+            repository: {
+                type: 'git',
+                url: 'https://github.com/dominique-mueller/test-library'
+            }
+        }, null, '  ' ) );
+
+    } );
+
+    afterAll( () => {
+    } );
+
+    it ( 'should run through', () => {
+
+        git( projectPath )
+            .init();
+
+    } );
+
+} );
