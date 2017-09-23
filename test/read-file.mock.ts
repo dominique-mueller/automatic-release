85b4c732d9041ef2fae5f4c74e8fb6ffe8b00725
Dominique Müller <dominique.m.mueller@gmail.com>
2017-09-23 16:42:52 +0200

    Add package json step tests, extend information step tests

--- test/read-file.mock.ts
+++ test/read-file.mock.ts
@@ -4,7 +4,7 @@
  * @param fileContent - File content to return
  * @param [spy]       - Spy
  */
-export function setupReadFileMock( fileContent: any, spy?: any ): void {
+export function setupReadFileMock( fileContent: any, shouldFailReading: boolean = false, spy?: any ): void {
 
 	jest.doMock( '../src/utilities/read-file', () => {
 		return {
@@ -12,7 +12,11 @@ export function setupReadFileMock( fileContent: any, spy?: any ): void {
 				if ( spy ) {
 					spy( filePath, isWithinLibrary );
 				}
-				return fileContent;
+				if ( shouldFailReading ) {
+					throw new Error( 'An error occured.' );
+				} else {
+					return fileContent;
+				}
 			}
 		};
 	} );
