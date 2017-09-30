770d9a0c5ac58eaa292ffe56f4520a97cbb2c57e
Dominique Müller <dominique.m.mueller@gmail.com>
2017-09-30 18:37:33 +0200

    Refactoring

---
+++ test/cases/first-release.spec.ts
@@ -0,0 +1,184 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as child_process from 'child_process';
+import { promisify } from 'util';
+
+import * as del from 'del';
+import * as git from 'simple-git';
+
+import { PackageJson } from '../../src/interfaces/package-json.interface';
+import { initGitCommits, GitConventionalCommit } from './../utilities/init-git-commits';
+import { initGitRepository } from './../utilities/init-git-repository';
+
+const readFileAsync = promisify( fs.readFile );
+const writeFileAsync = promisify( fs.writeFile );
+const mkdirAsync = promisify( fs.mkdir );
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 100000;
+
+/**
+ * Automatic Release: Unit Test for First Release
+ */
+describe( 'Automatic Release: First Release', () => {
+
+	const projectPath: string = path.resolve( process.cwd(), 'dist-test' );
+	let originalProcessCwd: any;
+	let commits: Array<GitConventionalCommit>;
+
+	// Setup
+	beforeAll( async() => {
+
+		jest.resetModules();
+
+		jest.doMock( 'child_process', () => {
+			return {
+
+				// Switch the working directory (used by 'git-raw-commits')
+				execFile: ( fileName: string, args: Array<string>, options: child_process.ExecFileOptions ): child_process.ChildProcess => {
+					const newOptions: child_process.ExecFileOptions = Object.assign( options, {
+						cwd: projectPath
+					} );
+					return child_process.execFile( fileName, args, newOptions );
+				},
+
+				// Simply forward (used by 'simple-git')
+				spawn: child_process.spawn
+
+			}
+		} );
+
+		// When loading JSON files, load them from the dist-test folder instead (used by changelog generator)
+		jest.doMock( './../../node_modules/conventional-changelog-core/node_modules/load-json-file', () => {
+			return async( filePath: string ): Promise<any> => {
+				const newFilePath: string = path.join( projectPath, path.relative( originalProcessCwd(), filePath ) );
+				const fileContent: string = JSON.parse( await readFileAsync( newFilePath, 'utf-8' ) );
+				return fileContent;
+			}
+		} );
+
+		// Hide logging output
+		jest.spyOn( console, 'log' ).mockImplementation( () => {
+			return;
+		} );
+
+		// Prepare folder (before changing working directory!!)
+		await del( projectPath );
+		await mkdirAsync( projectPath );
+
+		// Change working directory to test output folder
+		originalProcessCwd = process.cwd
+		process.cwd = () => projectPath;
+
+		// Prepare repository
+		await writeFileAsync( path.resolve( projectPath, 'package.json' ), JSON.stringify( getInitialPackageJson(), null, '	' ), 'utf-8' );
+		await initGitRepository( projectPath );
+		commits = await initGitCommits( projectPath, 'minor' ); // Will do 3 commits
+
+		// Run automatic release (the test cases will check the result)
+		const automaticRelease: () => Promise<void> = ( await import( './../../index' ) ).automaticRelease;
+		await automaticRelease();
+
+	} );
+
+	afterAll( () => {
+
+		process.cwd = originalProcessCwd;
+
+	} );
+
+	it ( 'should set the correct version in the "package.json" file', async() => {
+
+		// Read the package json file
+		const packageJsonFile: string = await readFileAsync( path.resolve( projectPath, 'package.json' ), 'utf-8' );
+		const packageJson: PackageJson = JSON.parse( packageJsonFile );
+
+		// Check the package.json file
+		expect( packageJson.version ).toBe( getInitialPackageJson().version ); // First version
+
+	} );
+
+	it ( 'should write the "CHANGELOG.md" file', async() => {
+
+		// Get date
+		const today: Date = new Date();
+		const todayFormatted: string = `${ today.getFullYear() }-${ today.getMonth() + 1 }-${ today.getDate() }`.replace( /(^|\D)(\d)(?!\d)/g, '$10$2' );
+
+		// Get changelog
+		const changelog: string = await readFileAsync( path.resolve( projectPath, 'CHANGELOG.md' ), 'utf-8' );
+		const [ changelogHeader, changelogContent, changelogFooter ]: Array<Array<string>> = parseChangelog( changelog );
+
+		// Check changelog header & footer
+		testChangelogHeader( changelogHeader, getInitialPackageJson().repository.url );
+		testChangelogFooter( changelogFooter );
+
+		// Check changelog content
+		expect( changelogContent[ 0 ] ).toBe( `## [${ getInitialPackageJson().version }](${ getInitialPackageJson().repository.url }/releases/tag/${ getInitialPackageJson().version }) / ${ todayFormatted }` );
+		expect( changelogContent[ 1 ] ).toBe( '' );
+		expect( changelogContent[ 2 ] ).toBe( '### Features' );
+		expect( changelogContent[ 3 ] ).toBe( '' );
+		testChangelogEntry( changelogContent[ 4 ], commits[ 1 ], getInitialPackageJson().repository.url );
+		expect( changelogContent[ 5 ] ).toBe( '' );
+		expect( changelogContent[ 6 ] ).toBe( '### Performance Improvements' );
+		expect( changelogContent[ 7 ] ).toBe( '' );
+		testChangelogEntry( changelogContent[ 8 ], commits[ 0 ], getInitialPackageJson().repository.url );
+
+	} );
+
+} );
+
+function testChangelogHeader( changelogHeader: Array<string>, repositoryUrl: string ): void {
+
+	expect( changelogHeader[ 0 ] ).toBe( '# Changelog' );
+	expect( changelogHeader[ 1 ] ).toBe( '' );
+	expect( changelogHeader[ 2 ] ).toBe( `Also see the **[release page](${ repositoryUrl }/releases)**.` );
+	expect( changelogHeader[ 3 ] ).toBe( '' );
+	expect( changelogHeader[ 4 ] ).toBe( '<br>' );
+	expect( changelogHeader[ 5 ] ).toBe( '' );
+
+}
+
+function testChangelogFooter( changelogFooter: Array<string> ): void {
+
+	expect( changelogFooter[ 0 ] ).toBe( '' );
+	expect( changelogFooter[ 1 ] ).toBe( '<br>' );
+	expect( changelogFooter[ 2 ] ).toBe( '' );
+	expect( changelogFooter[ 3 ] ).toBe( '---' );
+	expect( changelogFooter[ 4 ] ).toBe( '' );
+	expect( changelogFooter[ 5 ] ).toBe( '<sup>*Changelog generated automatically by [automatic-release](https://github.com/dominique-mueller/automatic-release).*</sup>' );
+	expect( changelogFooter[ 6 ] ).toBe( '' );
+
+}
+
+function testChangelogEntry( changelogContentLine: string, commit: GitConventionalCommit, repositoryUrl: string ): void {
+
+	const commitMessage: string = `* **${ commit.scope }:** ${ commit.message.split( '\n' )[ 0 ] }`;
+	const commitLink: string = `([${ commit.hash }](${ repositoryUrl }/commit/${ commit.hash }))`;
+
+	expect( changelogContentLine ).toBe( `${ commitMessage } ${ commitLink }` );
+
+}
+
+function parseChangelog( changelog: string ): Array<Array<string>> {
+	const changelogLines: Array<string> = splitByLinebreak( changelog );
+	return [
+		changelogLines.slice( 0, 6 ),
+		changelogLines.slice( 6, -7 ),
+		changelogLines.slice( -7 )
+	];
+}
+
+function splitByLinebreak( content: string ): Array<string> {
+	return content.split( /\r?\n/ );
+}
+
+function getInitialPackageJson(): PackageJson {
+	return  {
+		name: 'automatic-release-test',
+		description: 'Lorem ipsum dolor sit amet.',
+		version: '1.0.0',
+		repository: {
+			type: 'git',
+			url: 'https://github.com/dominique-mueller/automatic-release-test'
+		}
+	}
+}
--- test/utilities/init-git-commits.ts
+++ test/utilities/init-git-commits.ts
@@ -1,30 +1,69 @@
 import * as fs from 'fs';
 import * as path from 'path';
+import { promisify } from 'util';
 
 import * as git from 'simple-git';
+
+const writeFileAsync = promisify( fs.writeFile );
+
+/**
+ * Git Conventional Commit interface
+ */
+export interface GitConventionalCommit {
+	type?: string;
+	scope?: string;
+	message: string;
+	hash?: string;
+}
+
+/**
+ * Git Commit Log interface
+ */
+export interface GitCommitLog {
+	hash: string;
+	date: string; // In the format '2017-09-30 12:30:15 +0200'
+	message: string;
+	author_name: string;
+	author_email: string;
+}
 
 /**
  * Make Git Changes
  */
-export function initGitCommits( projectPath: string, type: 'major' | 'minor' | 'patch' | 'none' ): Promise<void> {
-	return new Promise( async( resolve: () => void, reject: () => void ): Promise<void> => {
+export function initGitCommits( projectPath: string, type: 'major' | 'minor' | 'patch' | 'none' ): Promise<Array<GitConventionalCommit>> {
+	return new Promise( async( resolve: ( commits: Array<GitConventionalCommit> ) => void, reject: () => void ): Promise<void> => {
 
+		// Do commits
+		const conventionalCommits: Array<GitConventionalCommit> = [];
 		await commitForNone( projectPath );
 		if ( type === 'patch' || type === 'minor' || type === 'major' ) {
-			await commitForPatch( projectPath );
+			conventionalCommits.push( await commitForPatch( projectPath ) );
 		}
 		if ( type === 'minor' || type === 'major' ) {
-			await commitForMinor( projectPath );
+			conventionalCommits.push( await commitForMinor( projectPath ) );
 		}
 		if ( type === 'major' ) {
-			await commitForMajor( projectPath );
+			conventionalCommits.push( await commitForMajor( projectPath ) );
 		}
 
-		git( projectPath )
-			.push( 'origin', 'develop' )
-			.exec( () => {
-				resolve();
+		// Push commits
+		await new Promise<void>( ( resolve: () => void, reject: () => void ): void => {
+			git( projectPath )
+				.push( 'origin', 'develop' )
+				.exec( () => {
+					resolve();
+				} );
+		} );
+
+		// Add commit hashes
+		const gitCommits: Array<GitCommitLog> = ( await getGitCommits( projectPath ) ).slice( 2 ); // Ignore non-conventional commits
+		conventionalCommits
+			.map( ( conventionalCommit: GitConventionalCommit, index: number ) => {
+				conventionalCommit.hash = gitCommits[ index ].hash.substring( 0, 7 );
+				return conventionalCommit;
 			} );
+
+		resolve( conventionalCommits );
 
 	} );
 }
@@ -32,16 +71,24 @@ export function initGitCommits( projectPath: string, type: 'major' | 'minor' | '
 /**
  * Commit for none
  */
-function commitForNone( projectPath: string ): Promise<void> {
-	return new Promise( ( resolve: () => void, reject: () => void ): void => {
+function commitForNone( projectPath: string ): Promise<GitConventionalCommit> {
+	return new Promise( async( resolve: ( commit: GitConventionalCommit ) => void, reject: () => void ): Promise<void> => {
 
-		fs.writeFileSync( path.resolve( projectPath, 'CONTRIBUTORS.md' ), '# TODO', 'utf-8' );
+		// Make changes
+		const changedFile: string = 'CONTRIBUTORS.md';
+		await writeFileAsync( path.resolve( projectPath, changedFile ), '# Contributors', 'utf-8' );
 
+		// Prepare commit
+		const commit: GitConventionalCommit = {
+			message: 'Add contributors file'
+		};
+
+		// Execute commit
 		git( projectPath )
-			.add( 'CONTRIBUTORS.md' )
-			.commit( 'Add contributors file' )
+			.add( changedFile )
+			.commit( buildCommitMessage( commit ) )
 			.exec( () => {
-				resolve();
+				resolve( commit );
 			} );
 
 	} );
@@ -50,16 +97,26 @@ function commitForNone( projectPath: string ): Promise<void> {
 /**
  * Commit for path
  */
-function commitForPatch( projectPath: string ): Promise<void> {
-	return new Promise( ( resolve: () => void, reject: () => void ): void => {
+function commitForPatch( projectPath: string ): Promise<GitConventionalCommit> {
+	return new Promise( async( resolve: ( commit: GitConventionalCommit ) => void, reject: () => void ): Promise<void> => {
 
-		fs.writeFileSync( path.resolve( projectPath, 'select.js' ), '// Select', 'utf-8' );
+		// Make changes
+		const changedFile: string = 'select.js';
+		await writeFileAsync( path.resolve( projectPath, changedFile ), '// Select', 'utf-8' );
 
+		// Prepare commit
+		const commit: GitConventionalCommit = {
+			type: 'perf',
+			scope: 'select',
+			message: 'Improve rendering performance for select options loop'
+		};
+
+		// Execute commit
 		git( projectPath )
-			.add( 'select.js' )
-			.commit( 'perf(select): Improve rendering performance for select options loop' )
+			.add( changedFile )
+			.commit( buildCommitMessage( commit ) )
 			.exec( () => {
-				resolve();
+				resolve( commit );
 			} );
 
 	} );
@@ -68,16 +125,26 @@ function commitForPatch( projectPath: string ): Promise<void> {
 /**
  * Commit for minor
  */
-function commitForMinor( projectPath: string ): Promise<void> {
-	return new Promise( ( resolve: () => void, reject: () => void ): void => {
+function commitForMinor( projectPath: string ): Promise<GitConventionalCommit> {
+	return new Promise( async( resolve: ( commit: GitConventionalCommit ) => void, reject: () => void ): Promise<void> => {
 
-		fs.writeFileSync( path.resolve( projectPath, 'input.js' ), '// Input', 'utf-8' );
+		// Make changes
+		const changedFile: string = 'input.js';
+		await writeFileAsync( path.resolve( projectPath, changedFile ), '// Input', 'utf-8' );
 
+		// Prepare commit
+		const commit: GitConventionalCommit = {
+			type: 'feat',
+			scope: 'input',
+			message: 'Add input component\n\n- Add input component implementation\n- Add unit tests\n- Add documentation'
+		};
+
+		// Execute commit
 		git( projectPath )
-			.add( 'input.js' )
-			.commit( 'feat(input): Add input component\n\n- Add input component implementation\n- Add unit tests\n- Add documentation' )
+			.add( changedFile )
+			.commit( buildCommitMessage( commit ) )
 			.exec( () => {
-				resolve();
+				resolve( commit );
 			} );
 
 	} );
@@ -86,16 +153,63 @@ function commitForMinor( projectPath: string ): Promise<void> {
 /**
  * Commit for major
  */
-function commitForMajor( projectPath: string ): Promise<void> {
-	return new Promise( ( resolve: () => void, reject: () => void ): void => {
+function commitForMajor( projectPath: string ): Promise<GitConventionalCommit> {
+	return new Promise( async( resolve: ( commit: GitConventionalCommit ) => void, reject: () => void ): Promise<void> => {
 
-		fs.writeFileSync( path.resolve( projectPath, 'textarea.js' ), '// Textarea', 'utf-8' );
+		// Make changes
+		const changedFile: string = 'textarea.js';
+		await writeFileAsync( path.resolve( projectPath, changedFile ), '// Textarea', 'utf-8' );
+
+		// Prepare commit
+		const commit: GitConventionalCommit = {
+			type: 'refactor',
+			scope: 'textarea',
+			message: 'Rename "maxlength" attribute to "maxLength"\n\nBREAKING CHANGE: The textarea "maxlength" attribute is now called "maxLength"'
+		};
+
+		// Execute commit
+		git( projectPath )
+			.add( changedFile )
+			.commit( buildCommitMessage( commit ) )
+			.exec( () => {
+				resolve( commit );
+			} );
+
+	} );
+}
+
+/**
+ * Build a commit message based on a commit object
+ *
+ * @param   commit - Commit object
+ * @returns        - Commit message
+ */
+function buildCommitMessage( commit: GitConventionalCommit ): string {
+	if ( commit.type ) {
+		return `${ commit.type }(${ commit.scope ? commit.scope : '*' }): ${ commit.message }`;
+	} else {
+		return commit.message;
+	}
+}
+
+
+/**
+ * Get all git commits
+ *
+ * @param   projectPath - Project path
+ * @returns             - List of git commits
+ */
+function getGitCommits( projectPath ): Promise<Array<GitCommitLog>> {
+	return new Promise( ( resolve: ( commits: Array<GitCommitLog> ) => void, reject: () => void ): void => {
 
 		git( projectPath )
-			.add( 'textarea.js' )
-			.commit( 'refactor(textarea): Rename "maxlength" attribute to "maxLength"\n\nBREAKING CHANGE: The textarea "maxlength" attribute is now called "maxLength"' )
-			.exec( () => {
-				resolve();
+			.checkout( 'develop' )
+			.log( ( error: Error | null, data: {
+				all: Array<GitCommitLog>;
+				latest: GitCommitLog,
+				total: number
+			} ) => {
+				resolve( data.all.reverse() );
 			} );
 
 	} );
