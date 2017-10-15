4edc6e87d5dba109ceac5d06679be9a77f0b1cc7
Dominique Müller <dominique.m.mueller@gmail.com>
2017-10-15 16:54:59 +0200

    Remove package json correction

--- src/steps/information.ts
+++ src/steps/information.ts
@@ -19,127 +19,60 @@ import { writeFile } from './../utilities/write-file';
  *
  * @returns - Promise, resolves with information
  */
-export function collectInformation(): Promise<AutomaticReleaseInformation> {
-	return new Promise( async( resolve: ( information: AutomaticReleaseInformation ) => void, reject: ( error: Error ) => void ) => {
+export async function collectInformation(): Promise<AutomaticReleaseInformation> {
 
-		try {
+	// Read, correct and write the package.json file
+	log( 'substep', 'Read the "package.json" file' );
+	const packageJson: PackageJson = await readFile( 'package.json' );
+	log( 'substep', 'Validate the "package.json" file' );
+	validateAndCorrectPackageJson( packageJson );
 
-			// Read, correct and write the package.json file
-			log( 'substep', 'Read the "package.json" file' );
-			const originalPackageJson: PackageJson = await readFile( 'package.json' );
-			console.log( originalPackageJson );
-			log( 'substep', 'Validate & correct (if necessary) the "package.json" file' );
-			const packageJson: PackageJson = await validateAndCorrectPackageJson( originalPackageJson );
-			log( 'substep', 'Write the updated "package.json" file' );
-			await writeFile( 'package.json', packageJson );
+	const information: AutomaticReleaseInformation = {};
 
-			const information: AutomaticReleaseInformation = {};
+	// Get version information
+	log( 'substep', 'Retrieve versioning details' );
+	information.isFirstVersion = !( await hasGitTags() );
+	information.oldVersion = packageJson.version;
+	information.newVersion = information.isFirstVersion ? information.oldVersion : await getNewVersion( information.oldVersion );
 
-			// Get version information
-			log( 'substep', 'Retrieve versioning details' );
-			information.isFirstVersion = !( await hasGitTags() );
-			information.oldVersion = packageJson.version;
-			information.newVersion = information.isFirstVersion ? information.oldVersion : await getNewVersion( information.oldVersion );
+	// Get repository information
+	log( 'substep', 'Get repository information' );
+	const githubUrl: GithubUrl = parseGithubUrl( packageJson.repository.url );
+	information.repositoryOwner = githubUrl.owner;
+	information.repositoryName = githubUrl.name;
+	information.repositoryUrl = githubUrl.href;
 
-			// Get repository information
-			log( 'substep', 'Get repository information' );
-			const githubUrl: GithubUrl = parseGithubUrl( packageJson.repository.url );
-			information.repositoryOwner = githubUrl.owner;
-			information.repositoryName = githubUrl.name;
-			information.repositoryUrl = githubUrl.href;
+	// Get GitHub authorization details
+	log( 'substep', 'Read & validate GitHub API token' );
+	information.githubToken = await getGithubToken( information.repositoryOwner, information.repositoryName );
 
-			// Get GitHub authorization details
-			log( 'substep', 'Read & validate GitHub API token' );
-			information.githubToken = await getGithubToken( information.repositoryOwner, information.repositoryName );
+	return information;
 
-			resolve( information );
-
-		} catch ( error ) {
-			reject( error );
-			return;
-		}
-
-	} );
 }
 
 /**
- * Try to validate and - if necessary / possible also correct - the package json content
+ * Validate the package json content
  *
- * @param   content - Original package json content
- * @returns         - Corrected package json content
+ * @param packageJson - Package json content
  */
-function validateAndCorrectPackageJson( content: PackageJson ): Promise<PackageJson> {
-	return new Promise<PackageJson>( async( resolve: ( correctedContent: PackageJson ) => void, reject: ( error: Error ) => void ) => {
+function validateAndCorrectPackageJson( packageJson: PackageJson ): void {
 
-		const correctedContent: PackageJson = content;
+	// Check the 'version' field
+	if ( !packageJson.hasOwnProperty( 'version' ) ) {
+		throw new Error( `The "package.json" file does not have a version defined.` );
+	}
+	if ( semver.valid( packageJson.version ) === null ) { // 'null' means invalid
+		throw new Error( `The "package.json" file defines the version "${ packageJson.version }"; regarding semantic versioning this is not a valid version number.` );
+	}
+	if ( semver.prerelease( packageJson.version ) !== null ) { // 'null' means no pre-release components exist
+		throw new Error( `The "package.json" file defines the version "${ packageJson.version }"; pre-release versions are not supported by automatic-release.` );
+	}
 
-		// Check the 'version' field
-		if ( !correctedContent.hasOwnProperty( 'version' ) ) {
-			correctedContent.version = '1.0.0';
-			log( 'note', 'There was no version defined in the "package.json" file, thus "1.0.0" is assumed.' );
-		}
-		if ( semver.valid( correctedContent.version ) === null ) { // 'null' means invalid
-			reject( new Error( `The "package.json" file defines the version "${ correctedContent.version }"; regarding semantic versioning this is not a valid version number.` ) );
-			return;
-		}
-		if ( semver.prerelease( correctedContent.version ) !== null ) { // 'null' means no pre-release components exist
-			reject( new Error( `The "package.json" file defines the version "${ correctedContent.version }"; pre-release versions are not supported by automatic-release.` ) );
-			return;
-		}
+	// Check the 'repository' field
+	if ( !packageJson.hasOwnProperty( 'repository' ) || !packageJson.repository.hasOwnProperty( 'url' ) ) {
+		throw new Error( 'The "package.json" file defines no repository URL (and retrieving it from the Git project configuration failed).' );
+	}
 
-		// Check the 'repository' field
-		if ( !correctedContent.hasOwnProperty( 'repository' ) ) {
-			correctedContent.repository = {
-				type: 'git'
-			};
-		}
-		if ( !correctedContent.repository.hasOwnProperty( 'url' ) ) {
-			try {
-				correctedContent.repository.url = await getGitRemoteUrl();
-				log( 'note', `There was no repository URL defined in the "package.json" file, thus "${ correctedContent.repository.url }" is assumed.` );
-			} catch ( repositoryUrlError ) {
-				// We don't really care about the actual reason or this error, this here just would have been nice for auto-correction
-				reject( new Error( 'The "package.json" file defines no repository URL (and retrieving it from the Git project configuration failed).' ) );
-				return;
-			}
-		}
-
-		resolve( correctedContent );
-
-	} );
-}
-
-/**
- * Get the Git remote URL from the git project settings / configuration (if possible)
- *
- * @returns - Promise, resolves with the git remote URL
- */
-function getGitRemoteUrl(): Promise<string> {
-	return new Promise<string>( ( resolve: ( url: string ) => void, reject: ( error: Error ) => void ) => {
-
-		// Get all remotes (verbose=true also gives us the URLs)
-		git( process.cwd() )
-			.getRemotes( true, ( gitGetRemotesError: Error | null, gitRemotes: Array<GitRemote> ) => {
-
-				// Handle errors
-				if ( gitGetRemotesError ) {
-					reject( new Error( `An error has occured while retrieving the project's Git remotes. [${ gitGetRemotesError.message }]` ) );
-					return;
-				}
-
-				// List of git remotes is '[ { name: '', refs: {} } ]' wheh no remotes are available
-				if ( gitRemotes[ 0 ].name === '' || !gitRemotes[ 0 ].refs.hasOwnProperty( 'fetch' ) ) {
-					reject( new Error( 'No remotes for the Git project found.' ) );
-					return;
-				}
-
-				// Return the normalized git path
-				const normalizedGitPath: string = gitRemotes[ 0 ].refs.fetch.replace( '.git', '' );
-				resolve( normalizedGitPath );
-
-			} );
-
-	} );
 }
 
 /**
