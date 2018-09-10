7db04af823ba41934435cdec6bfa2b4566a71231
Dominique <dominique.m.mueller@gmail.com>
2018-09-10 10:43:06 +0200

    Simplify changelog generation

--- src/steps/changelog.ts
+++ src/steps/changelog.ts
@@ -1,6 +1,7 @@
 import * as path from 'path';
 import * as stream from 'stream';
 
+import * as getStream from 'get-stream';
 import * as conventionalChangelog from 'conventional-changelog';
 
 import { changelogTransformer, changelogCommitGroupsSort } from './../templates/changelog-transform';
@@ -36,59 +37,31 @@ export class ChangelogGenerator {
 	private async generateChangelogContent( repositoryUrl: string ): Promise<string> {
 		return new Promise<string>( async ( resolve: ( changelogContent: string ) => void, reject: ( error: Error ) => void ) => {
 
-			// Read in changelog template files
-			const changelogChunks: Array<string> = [];
-
-			// Header information
-			changelogChunks.push( '# Changelog\n\n' ); // Title
-			changelogChunks.push( `Also see the **[release page](${ repositoryUrl }/releases)**.\n` ); // Sub-title (Github-specific)
-
-			// Generate changelog
-			const changelogStream: stream.Readable = conventionalChangelog( { // package.json file has to be updated before
-				pkg: {
-					path: path.resolve( process.cwd(), 'package.json' )
-				},
-				preset: 'angular',
-				releaseCount: 0 // Regenerate the whole thing every time
-			}, {
+			const changelogHeader: string = `# Changelog\n\nAlso see the **[release page](${ repositoryUrl }/releases)**.\n`;
+			const changelogFooter: string = '\n<br>\n\n---\n\n<sup>*Changelog generated automatically by [automatic-release](https://github.com/dominique-mueller/automatic-release).*</sup>\n';
+			const changelogBody: string = await getStream(
+				conventionalChangelog( {
+					pkg: {
+						path: path.resolve( process.cwd(), 'package.json' )
+					},
+					preset: 'angular',
+					releaseCount: 0 // Regenerate the whole thing every time
+				}, {
 					linkCompare: false // We use a custom link
 				}, {}, {}, {
 					commitGroupsSort: changelogCommitGroupsSort,
-					commitPartial: await this.readTemplate( 'commit' ),
-					footerPartial: await this.readTemplate( 'footer' ),
-					headerPartial: await this.readTemplate( 'header' ),
-					mainTemplate: await this.readTemplate( 'main' ),
-					transform: changelogTransformer( repositoryUrl ) // Custom transform (shows all commit types)
-				} );
+					commitPartial: await readFile( `./../templates/changelog-commit.hbs`, true ),
+					footerPartial: await await readFile( `./../templates/changelog-footer.hbs`, true ),
+					headerPartial: await await readFile( `./../templates/changelog-header.hbs`, true ),
+					mainTemplate: await await readFile( `./../templates/changelog-main.hbs`, true ),
+					transform: changelogTransformer( repositoryUrl ) // Custom transform
+				} )
+			);
 
-			// Handle errors
-			changelogStream.on( 'error', ( error: Error ) => {
-				reject( new Error( `An error occured while generating the changelog. [${ error.message }]` ) );
-				return;
-			} );
-
-			// Handle incoming data
-			changelogStream.on( 'data', ( dataChunk: string ) => {
-				changelogChunks.push( dataChunk );
-			} );
-
-			// Handle finish
-			changelogStream.on( 'end', () => {
-
-				// Footer information
-				changelogChunks.push( '\n<br>\n\n---\n\n<sup>*Changelog generated automatically by [automatic-release](https://github.com/dominique-mueller/automatic-release).*</sup>\n' );
-
-				// Create the full changelog
-				const changelog: string = changelogChunks.join( '' );
-				resolve( changelog );
-
-			} );
+			const changelog: string = `${ changelogHeader }${ changelogBody }${ changelogFooter }`;
+			resolve( changelog );
 
 		} );
-	}
-
-	private async readTemplate( templateName: string ): Promise<string> {
-		return readFile( `./../templates/changelog-${ templateName }.hbs`, true );
 	}
 
 }
